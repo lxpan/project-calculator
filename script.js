53e4939211405b9ae38e25fe17381515b7ad1c5d
Luoxi Pan <l.panpax@gmail.com>
2022-04-08 19:21:07 +1000

    Declare functions of basic arithmetic

---
+++ script.js
@@ -0,0 +1,20 @@
+/*
+Project: Calculator
+Author: Luoxi Pan
+*/
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
